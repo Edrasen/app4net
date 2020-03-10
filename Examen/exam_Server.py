d580a4a5794ba9cb2145b0962543a8516226e8eb
Omar1Cruz271 <omar1cruz2714721@gmail.com>
2020-03-10 15:20:29 -0600

    Se hizo por concepto

--- Examen/exam_Server.py
+++ Examen/exam_Server.py
@@ -3,8 +3,9 @@
 Created on Mon Mar  2 09:49:26 2020
 
 @author: omar-
-         edrasen
 """
+
+
 import socket
 import random
 import pickle
@@ -38,93 +39,116 @@ class palabras:
             self.escuela()
     def escuela(self):
         print("me meti")
-        self.palabra.append("libros")
-        self.palabra.append("apuntes")
-        self.palabra.append("colores")
-        self.palabra.append("pizarron")
-        self.palabra.append("plumones")
-        self.palabra.append("alumnos")
-        self.palabra.append("profesores")
-        self.palabra.append("salones")
-        self.palabra.append("bancas")
-        self.palabra.append("lapicero")
+        self.palabra.append("LIBROS")
+        self.palabra.append("APUNTES")
+        self.palabra.append("COLORES")
+        self.palabra.append("PIZARRON")
+        self.palabra.append("PLUMONES")
+        self.palabra.append("ALUMNOS")
+        self.palabra.append("PROFESORES")
+        self.palabra.append("SALONES")
+        self.palabra.append("BANCAS")
+        self.palabra.append("CUADERNO")
         #print(self.palabra)
 
 
 def compruebaEspacios(palabra,matriz, ocupados, tamaño, x, y):
     seEscribio=False
     deshacer=1
-    no_located = []
+    pos=1
+    inifin=[]
     if x+tamaño<14:#para la validacion vertical se usa x porque son los renglones
         for i in palabra:
             if [x,y] in ocupados and matriz[x][y]!=i:
                 for m in range(deshacer):
                     ocupados.pop(len(ocupados)-1)
-                    #if i not in no_located
-                return False, matriz,ocupados
+                return False, matriz,ocupados,None
             matriz[x][y]=i
+            if pos==1:
+                inifin.append(x)
+                inifin.append(y)
+                
             ocupados.append([x,y])
             deshacer+=1
+            pos+=1
             x+=1
+        inifin.append(x-1)
+        inifin.append(y)
         seEscribio=True
-
-    elif y+tamaño<14:
-        for i in palabra:
-            if [x,y] in ocupados and matriz[x][y]!=i:
-                for m in range(deshacer):
-                    ocupados.pop(len(ocupados)-1)
-                return False, matriz,ocupados
-            matriz[x][y]=i
-            ocupados.append([x,y])
-            deshacer+=1
-            y+=1
-        seEscribio=True
-
+            
     elif x-tamaño>0:
         for i in palabra:
             if [x,y] in ocupados and matriz[x][y]!=i:
                 for m in range(deshacer):
                     ocupados.pop(len(ocupados)-1)
-                return False, matriz,ocupados
+                return False, matriz,ocupados,None
             matriz[x][y]=i
+            if pos==1:
+                inifin.append(x)
+                inifin.append(y)
             ocupados.append([x,y])
             deshacer+=1
+            pos+=1
             x-=1
+        inifin.append(x+1)
+        inifin.append(y)
         seEscribio=True
-
+    elif y+tamaño<14:
+        for i in palabra:
+            if [x,y] in ocupados and matriz[x][y]!=i:
+                for m in range(deshacer):
+                    ocupados.pop(len(ocupados)-1)
+                return False, matriz,ocupados,None
+            matriz[x][y]=i
+            if pos==1:
+                inifin.append(x)
+                inifin.append(y)
+            ocupados.append([x,y])
+            deshacer+=1
+            pos+=1
+            y+=1
+        inifin.append(x)
+        inifin.append(y-1)
+        seEscribio=True
     elif y-tamaño>0:
         for i in palabra:
             if [x,y] in ocupados and matriz[x][y]!=i:
                 for m in range(deshacer):
                     ocupados.pop(len(ocupados)-1)
-                return False, matriz,ocupados
+                return False, matriz,ocupados,None
             matriz[x][y]=i
+            if pos==1:
+                inifin.append(x)
+                inifin.append(y)
             ocupados.append([x,y])
-            y-=1
             deshacer+=1
+            pos+=1
+            y-=1
+        inifin.append(x)
+        inifin.append(y+1)
         seEscribio=True
-    else:
-        compruebaEspacios(no_located, matriz,ocupados,random.randint(0,14), random.randit(0,14))
-    print(ocupados)
-    return seEscribio, matriz, ocupados
+    print("inifin",inifin)
+    return seEscribio, matriz, ocupados, inifin
    
     
       
 def mezclaPalabras(matriz, palabras):
     ocupados=[]
+    posiciones=[]
     seEscribio=False
     for i in range(len(palabras)):
         while seEscribio==False:
             x=random.randint(0,14)
             y=random.randint(0,14)
-            seEscribio,matriz, ocupados=compruebaEspacios(palabras[i],matriz, ocupados,len(palabras[i])-1,x,y)
+            seEscribio,matriz, ocupados,inifin=compruebaEspacios(palabras[i],matriz, ocupados,len(palabras[i]),x,y)
+            posiciones.append(inifin)
         if i!= len(palabras)-1:
             seEscribio=False
     for i in range(15):
         print(matriz[i])
     print("\n\n")
-    
-    return matriz
+    print(posiciones)
+    return matriz, posiciones
         
             
             
@@ -137,9 +161,11 @@ def main():
     dato=matriz.m
     dato2=palabras(random.randint(0,4));
     enviar2=dato2.palabra
-    dato=mezclaPalabras(dato, enviar2)
+    dato,posiciones=mezclaPalabras(dato, enviar2)
     print(enviar2)
-    dato=pickle.dumps(dato);
+    dato=pickle.dumps(dato)
+    posiciones=pickle.dumps(posiciones)
+    enviar2=pickle.dumps(enviar2)
     for i in range(15):
         print(matriz.m[i])
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -150,5 +176,6 @@ def main():
             print('Connected by', addr)
             while True:
                 conn.sendall(dato)
-               
+                conn.sendall(posiciones)
+                conn.sendall(pickle.dumps(dato2.palabra))
 main()
