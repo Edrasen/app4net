3ef86e985f509369b6028e1cfc02765f49c63d4a
Edgar Alain Ramos Mesas <alain42117@hotmail.com>
2020-06-18 17:21:44 -0500

    Update clientV2_GUI.py

--- Chat_MCAST_NB/clientV2_GUI.py
+++ Chat_MCAST_NB/clientV2_GUI.py
@@ -5,6 +5,7 @@ from threading import Thread
 cliente = clientV2.sender("224.1.1.1", 5007,2)
 name = input("Introduzca su nombre: ")
 
+
 def send(event=None):
     msg = my_msg.get()
     my_msg.set("") 
@@ -39,6 +40,8 @@ scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack()
+my_msg.set("{hi}")
+send()
 
 entry_field = tkinter.Entry(top, textvariable=my_msg)
 entry_field.bind("<Return>", send)
@@ -47,6 +50,7 @@ send_button = tkinter.Button(top, text="Send", command=send)
 send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
+
 
 receive_thread = Thread(target=recv)
 receive_thread.daemon = True
